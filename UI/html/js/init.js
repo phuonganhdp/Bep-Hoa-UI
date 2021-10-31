10628999dd5c6dad1094225a2e89458f8b0ac1a4
manhnguyennn <60807101+manhnguyennn@users.noreply.github.com>
2021-10-31 16:58:17 +0700

    Test

---
+++ UI/html/js/init.js
@@ -0,0 +1,43 @@
+﻿let App = {
+    windowW: $(window).width(),
+    initHelpers: function($helpers) {
+        let me = this;
+        $(document).ready(function() {
+            me.initHelper('common');
+            if ($helpers != undefined) {
+                if ($helpers instanceof Array) {
+                    for (let $index in $helpers) {
+                        me.initHelper($helpers[$index]);
+                    }
+                } else {
+                    me.initHelper($helpers);
+                }
+            } else {
+
+            }
+        });
+
+    },
+    initHelper: function($helper) {
+        let me = this;
+        //console.log($helper);
+        if ($helper.length > 0) {
+            console.log('init <' + $helper + '> function window width = ' + me.windowW);
+            App[$helper]();
+        }
+
+    },
+
+    common: function() {
+        let me = this;
+        //$('.timeago').timeago();
+
+        if (me.windowW > 800) {
+
+        } else {
+
+
+        }
+    },
+
+};
